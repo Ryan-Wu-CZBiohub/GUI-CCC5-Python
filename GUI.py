4d36d17b2ef122375571a5f16ea8be06bca8b2af
Ryan-Wu-CZBiohub <ryan.wu@czbiohub.org>
2025-07-23 09:27:56 -0500

    test git update

--- GUI.py
+++ GUI.py
@@ -19,6 +19,7 @@ from PySide6.QtWidgets import (
     QFileDialog,
     QMainWindow,
     QDialog,
+    QSpinBox,
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPalette, QColor, QIcon, QTextCursor, QKeySequence, QPixmap
