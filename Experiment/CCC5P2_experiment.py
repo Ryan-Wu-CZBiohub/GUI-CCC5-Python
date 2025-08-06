b8027f5345813897d55d3367adb60046b3f80605
Ryan-Wu-CZBiohub <ryan.wu@czbiohub.org>
2025-08-06 17:00:22 -0500

    renaming file

--- Experiment/CCC5P2_experiment.py
+++ Experiment/CCC5P2_experiment.py
@@ -22,7 +22,7 @@ except NameError:
 
 sys.path.append(os.path.abspath(os.path.join(BASE_DIR, '..')))
 from Connection.Connection import Connection
-from experiment_config import (
+from Experiment_Config import (
     EXPERIMENT_NAME,
     EXPERIMENT_TOTAL_TIME,
     EXPERIMENT_CONFIG,
